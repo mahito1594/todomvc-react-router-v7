8ef273d3bbad522c9c2599f3b68b7202260d5a2c
Mahito TANNO <mahito@presche.me>
2025-02-28 22:44:26 +0900

    chore:  modify basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { BASENAME } from "vite.config";
 
 export default {
   // enable SPA mode
   ssr: false,
+  basename: BASENAME ? `/${BASENAME}/` : "/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// To deploy to GitHub Pages, we need to set the base path to the repository name
+export const BASENAME = "todomvc-react-router-v7";
+
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  base: `/${BASENAME}/`,
 });
