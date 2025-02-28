86770e58de351fcbcdd32ebed73bd718638f4055
Mahito TANNO <mahito@presche.me>
2025-02-28 22:11:32 +0900

    feat:  modify title & check complete
    TODO
    - 新規作成機能
    - 削除機能
    - データの永続化

---
+++ app/data/todoRepository.client.ts
@@ -0,0 +1,44 @@
+class FakeTodoRepository {
+  private todos;
+  private nextId: number;
+  constructor() {
+    this.todos = [
+      { id: 0, title: "Learn React", completed: true },
+      { id: 1, title: "Learn React Router", completed: true },
+      { id: 2, title: "Learn Remix", completed: false },
+    ];
+    this.nextId = 3;
+  }
+
+  async getAll() {
+    return this.todos;
+  }
+
+  async get(id: number) {
+    return this.todos.find((todo) => todo.id === id);
+  }
+
+  async add(title: string) {
+    const todo = { id: this.nextId, title, completed: false };
+    this.todos.push(todo);
+    this.nextId++;
+    return todo;
+  }
+
+  async update(id: number, title: string, completed: boolean) {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      return;
+    }
+    todo.title = title;
+    todo.completed = completed;
+    return todo;
+  }
+
+  async destroy(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    return true;
+  }
+}
+
+export const todoRepository = new FakeTodoRepository();
