a5abd9b1172f64e66c0d821581de8636bba16b34
Mahito TANNO <mahito@presche.me>
2025-03-01 16:13:02 +0900

    feat:  add filter by todo.completed

--- app/components/todoList.tsx
+++ app/components/todoList.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router";
 import type { TodoListItemProps } from "./todoListItem";
 import TodoListItem from "./todoListItem";
 
@@ -6,10 +7,36 @@ export type TodoListProps = {
 };
 
 export default function TodoList({ todos }: TodoListProps) {
+  const [searchParams] = useSearchParams();
+  const filterMode = searchParams.get("filter") || "all";
+  const visibleTodos = todos.filter((todo) => {
+    switch (filterMode) {
+      case "all":
+        return true;
+      case "active":
+        return !todo.completed;
+      case "completed":
+        return todo.completed;
+      default:
+        return true;
+    }
+  });
+
   return (
     <main className="main">
+      {visibleTodos.length > 0 && (
+        <div className="toggle-all-container">
+          <input
+            className="toggle-all"
+            type="checkbox"
+            id="toggle-all"
+            onChange={() => void 0} // TODO
+          />
+          <label htmlFor="toggle-all">Toggle All Input</label>
+        </div>
+      )}
       <ul className="todo-list">
-        {todos.map((todo) => (
+        {visibleTodos.map((todo) => (
           <TodoListItem {...todo} key={todo.id} />
         ))}
       </ul>
