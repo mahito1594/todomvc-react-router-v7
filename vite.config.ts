3a62b8838052591e653bf8bec1eef18c6b50d6e7
Mahito TANNO <mahito@presche.me>
2025-03-02 16:41:25 +0900

    test:  add test deps and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
+/// <reference types="vitest" />
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { coverageConfigDefaults } from "vitest/config";
 
 // To deploy to GitHub Pages, we need to set the base path to the repository name
 export const BASENAME = "todomvc-react-router-v7";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
   base: `/${BASENAME}/`,
+  test: {
+    environment: "happy-dom",
+    setupFiles: "./test/setup.ts",
+    coverage: {
+      include: ["app/**/*"],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "app/routes.ts",
+        "app/root.tsx",
+      ],
+    },
+  },
 });
