cb25e89c4fa621f08cf3f9a3358b17e973615501
Mahito TANNO <mahito@presche.me>
2025-03-02 22:04:17 +0900

    test: add data tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,21 @@
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, vi } from "vitest";
 import "@testing-library/jest-dom/vitest";
 
 afterEach(() => {
   // cleanup manually instead of set `globals: true`
   cleanup();
+});
+
+const mockLocalStorage = (() => {
+  const _store = new Map<string, string>();
+  return {
+    getItem: vi.fn((key: string) => _store.get(key)),
+    setItem: vi.fn((key: string, value: string) => _store.set(key, value)),
+    removeItem: vi.fn((key: string) => _store.delete(key)),
+    clear: vi.fn(() => _store.clear()),
+  };
+})();
+Object.defineProperty(window, "localStorage", {
+  value: mockLocalStorage,
 });
